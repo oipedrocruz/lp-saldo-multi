2c74846df0e2af319127d1cac94fd9abf95ec08b
MateusDuraessantos <mateusduraessantos@gmail.com>
2025-05-16 19:49:35 -0300

    Versão desktop

---
+++ script.js
@@ -0,0 +1,234 @@
+const data = {
+  collapse1: [
+    {
+      title: 'Quais são as duas primeiras novidades da Caju que estão chegando e qual o impacto para os colaboradores?',
+      htmlContent: `
+      <p>A Caju está lançando a <strong>Conta Digital</strong> e uma nova categoria de benefício, o <strong>Saldo Multi</strong>. A Conta Digital é uma conta pessoal que oferece Pix, boleto, extrato e saque, enquanto o Saldo Multi oferece mais flexibilidade, aceitando os benefícios previstos na CLT (saúde, cultura, mobilidade, educação e home office  e até alimentação). </p>
+      <br />
+      <p>Com essas duas novidades chegando, a categoria Saldo Livre será descontinuado em breve, mas não se preocupe, saldos remanescentes da categoria Saldo Livre ou da carteira Caju Premiações serão migrados automaticamente para a Conta Digital, proporcionando mais clareza e controle para os colaboradores, que terão liberdade para realizar transações.</p>
+
+      `
+    },
+    {
+      title: 'O que vai mudar nas categorias de benefícios da Caju?',
+      htmlContent: `
+        <p>
+          Nada, caso as categorias refeição e alimentação sejam benefícios oferecidos com base no PAT ou na CLT, permanecem do mesmo jeito, com restrições de uso sem possibilidade de transferência de saldos. Outras categorias podem ser configuradas com mais flexibilidade ou restrição de uso, de acordo com as preferências da empresa, sempre respeitando as regras dos estabelecimentos autorizados a transacionar em cada uma de acordo com a sua finalidade.
+        </p>
+      `
+    },
+  ],
+
+  collapse2: [
+    {
+      title: 'A Conta Digital é pessoal ou empresarial? Quais funcionalidades ela oferece?',
+      htmlContent: `A <strong>Conta Digital</strong> é pessoal do colaborador. Ela permite que o colaborador receba saldos na conta e use esse saldo com a funcionalidade de Pix (com toda segurança como opt-in e biometria), saque, pague boletos e acesse extratos, tudo sem restrições de estabelecimentos.`
+    },
+    {
+      title: 'O RH precisa ativar ou configurar algo para a conta?',
+      htmlContent: `Não, a criação da conta e a migração, se necessário de saldos remanescentes da categoria Saldo livre ou Caju Premiações serão automáticas com a disponibilidade da Conta Digital da Caju. O RH não precisa realizar nenhuma configuração específica.`
+    },
+    {
+      title: 'A empresa consegue ver o extrato da conta do colaborador?',
+      htmlContent: `Não, o extrato é privado e protegido pela Lei de Sigilo Bancário. Para controle da empresa, a Caju disponibiliza relatório indicando os valores depositados pelo RH da Empresa.`
+    },
+    {
+      title: 'A Conta Digital é opcional?',
+      htmlContent: `Não, a Conta Digital é parte do novo ecossistema de soluções da Caju e será automaticamente fornecida a todos os colaboradores ativos, não sendo possível desabilitá-la.`
+    },
+    {
+      title: 'Posso usar a Conta Digital para qualquer tipo de depósito?',
+      htmlContent: `Recomendamos o uso da Conta para ajudas de custo e auxílio-creche. Outras finalidades podem ser usadas desde que previamente avaliadas com o time jurídico e fiscal da empresa para evitar possíveis tributos ou encargos.`
+    },
+  ],
+
+  collapse3: [
+    {
+      title: 'O que é o Saldo Multi e como ele se compara ao Saldo Livre?',
+      htmlContent: `
+        <p>O <strong>Saldo Multi</strong> é uma categoria de benefício que oferece flexibilidade para as 8 categorias de benefícios da CLT (a saúde, home-office, educação, cultura, até alimentação, caso os valores depositados pelo RH já tenham sido consumidos), mas com controle de finalidade de uso. Ele, juntamente com a Conta Digital, vão elevar a experiência do Saldo Livre, substituindo esta categoria que será descontinuada.</p>
+        <br />
+        <p>
+          A categoria Saldo Multi não permite saques, mas permitirá o pagamento de boletos de consumo (conta de água, luz, eletricidade, etc), ao contrário da Conta Digital, que oferece total liberdade de uso.
+        </p>
+      `
+    },
+    {
+      title: 'É possível configurar limites no Saldo Multi?',
+      htmlContent: `Não, o Saldo Multi pode ser utilizado nas 8 categorias já liberadas, mas, caso o RH queira restringir o uso em categorias específicas, pode travar os saldos individualmente, como já acontece atualmente.`
+    },
+    {
+      title: 'O que muda para o produto Caju Premiações e Caju Despesas?',
+      htmlContent: `
+        <p>O <strong>Caju Premiações</strong> continuará disponível na plataforma da Caju para RH para depósitos de premiações, mas agora a Caju disponibilizará esses saldos aos colaboradores na <strong>Conta Digital</strong>.</p>
+        <br/>
+        <p>Quanto ao <strong>Caju Despesas</strong>, os reembolsos serão depositados diretamente na Conta Digital do colaborador, enquanto os adiantamentos da empresa serão depositados em uma carteira separada, pois o saldo pertence à empresa</p>
+      `
+    },
+  ],
+
+  collapse4: [
+    {
+      title: 'O que muda para o produto Caju Premiações e Caju Despesas?',
+      htmlContent: `
+        <p>O <strong>Caju Premiações</strong> continuará disponível na plataforma da Caju para RH para depósitos de premiações, mas agora a Caju disponibilizará esses saldos aos colaboradores na <strong>Conta Digital</strong>. </p>
+        <br/>
+        Quanto ao <strong>Caju Despesas</strong>, os reembolsos serão depositados diretamente na Conta Digital do colaborador, enquanto os adiantamentos da empresa serão depositados em uma carteira separada, pois o saldo pertence à empresa.
+        <p></p>
+      `
+    },
+    {
+      title: 'A empresa pode resgatar o valor de uma premiação ou reembolso?',
+      htmlContent: `Não. Após o depósito, o valor pertence ao colaborador. Em casos excepcionais, como premiações ainda não pagas ou pedidos pagos antes da ativação da conta pelo colaborador, o resgate será permitido.`
+    },
+    
+  ],
+  collapse5: [
+    {
+      title: 'Como o RH deve escolher onde depositar os saldos e posso continuar com as categorias tradicionais?',
+      htmlContent: `
+        <p>O RH pode optar por depositar na Conta Digital (para saldos que não são benefícios), no Saldo Multi (saldo de benefício com flexibilidade) ou nas categorias tradicionais (saldo de benefícios com regras específicas). As categorias tradicionais, como Alimentação e Cultura, continuarão disponíveis.</p>
+        <br/>
+        <p>Agora o RH tem mais possibilidades de fazer depósitos de saldos para os colaboradores. Para depositar saldo de benefícios regulados pelo PAT ou CLT, benefícios fixos como Alimentação e Refeição e os benefícios flexíveis com as categorias educação, mobilidade, cultura, etc, nada muda, o RH pode continuar fazendo os depósitos como sempre. </p>
+        <br/>
+       <p>A novidade é que agora o RH tem a opção de depositar saldo de benefício flexível na categoria Saldo Multi, uma única categoria que é aceita nas demais categorias flexíveis, assim o RH não precisa mais fazer a divisão de saldo entre as categorias flexíveis e o colaborador não vai precisar fazer transferência de saldo entre as categorias flexíveis quando necessário.</p>
+      `
+    },
+    {
+      title: 'A mudança afeta a folha de pagamento ou exige treinamento do time de RH?',
+      htmlContent: `A mudança não afeta a folha de pagamento, desde que a finalidade do depósito esteja clara. Para depósitos fora do escopo de benefícios, o RH deve consultar o time jurídico ou fiscal. Quanto ao treinamento, a navegação no portal da empresa não mudará, mas a Caju fornecerá materiais de apoio para facilitar a transição.`
+    },
+    {
+      title: 'O que o colaborador precisa fazer?',
+      htmlContent: `O colaborador precisa apenas ativar o Pix (opt-in) caso deseje usar essa funcionalidade na sua Conta Digital. A conta será criada automaticamente, e caso haja saldo remanescente da categoria Saldo Livre ou saldo de Caju premiações, esses saldos serão migrados para a conta`
+    },
+  ],
+
+  collapse6: [
+    {
+      title: 'Como posso obter suporte e onde posso acessar materiais de comunicação?',
+      htmlContent: `Você pode entrar em contato com seu gerente de conta, com o time de suporte da Caju ou acessar os materiais de ajuda dentro da plataforma da Caju para RH. A Caju também está preparando e-mails, textos, telas no app e um FAQ para comunicar as mudanças para todos os envolvidos.`
+    },
+    {
+      title: 'Existe um cronograma para essas mudanças?',
+      htmlContent: `Sim, o lançamento será feito em fases, e você será informado com antecedência sobre quando e como sua empresa será impactada.`
+    },
+  ],
+  /* Colaborador */
+  colaborador1: [
+    {
+      title: 'O que é a Conta Digital da Caju e o Saldo Multi?',
+      htmlContent: `A Conta Digital da Caju é uma conta pessoal integrada ao app, onde você pode receber saldos, fazer Pix, pagar boletos, sacar dinheiro e acompanhar tudo em um extrato completo. Já o Saldo Multi é uma nova categoria de benefício mais flexível, que pode ser usado em todas as 8 categorias de benefícios da Caju, como cultura, mobilidade, saúde e até alimentação.`
+    },
+    {
+      title: 'O que acontece com o saldo que eu tinha no Saldo Livre e nas premiações da empresa?',
+      htmlContent: `O saldo remanescente da categoria Saldo Livre ou Caju Premiações será migrado automaticamente para sua Conta Digital, permitindo maior liberdade de uso (Pix, boleto, saques ou transações com o Cartão Caju). Os novos saldos do Caju Premiações que a sua empresa depositar agora serão disponibilizadas na sua Conta Digital, dispensando a necessidade de troca de carteira para o uso do saldo, incluindo a realização de transações via Pix.`
+    },
+  ],
+  colaborador2: [
+    {
+      title: 'Preciso baixar outro aplicativo ou ativar a Conta Digital',
+      htmlContent: `Não é necessário baixar outro aplicativo, pois tudo acontece no app da Caju que você já utiliza. A Conta Digital será criada automaticamente. Para usar o Pix, basta ativá-lo com biometria.`
+    },
+    {
+      title: 'Onde posso ver o extrato da minha Conta Digital?',
+      htmlContent: `Você encontra o extrato completo da Conta Digital diretamente no app da Caju, na aba “Conta Digital”. Lá, é possível acompanhar todas as entradas e saídas de saldo.`
+    },
+    {
+      title: 'Como posso usar o saldo da Conta Digital?',
+      htmlContent: `
+        <p>Com a Conta Digital, você pode fazer Pix (após cadastro e ativação de biometria de segurança do pix), pagar boletos, sacar dinheiro (mediante taxa, se aplicável) e realizar compras com o Cartão Caju.</p>
+        <br/>
+        <p>Se houver saldo disponível em uma categoria de benefício compatível com a compra, o valor será debitado automaticamente dessa categoria. Caso contrário, o valor será cobrado da sua Conta Digital, mas caso não haja saldo na Conta Digital, a transação não será realizada.</p>
+      `
+    },
+    {
+      title: 'Existe limite de uso para Pix ou saques',
+      htmlContent: `Sim, existem limites de segurança, que podem ser consultados na área do Pix dentro da  Conta no app.`
+    },
+    {
+      title: 'Posso cancelar ou excluir minha Conta Digital?',
+      htmlContent: `A Conta Digital é parte do ecossistema de soluções da Caju e estará ativa enquanto você for colaborador de uma empresa que oferece os benefícios Caju. Isto é, qualquer pedido de encerramento de conta não se encerra apenas o produto de conta digital, mas também de benefícios, sendo necessária a ciência do empregador.`
+    },
+  ],
+  colaborador3: [
+    {
+      title: 'O Saldo Multi é igual à Conta Digital?',
+      htmlContent: `Não. O Saldo Multi é uma categoria de benefício flexível, ele é aceito em todas as 8 categorias de benefícios da Caju, como mobilidade, educação e saúde, entre outros. A Conta Digital, por sua vez, oferece total liberdade de uso, sem restrição de como outras contas digitais do mercado.`
+    },
+    {
+      title: 'Como vou saber qual saldo estou usando?',
+      htmlContent: `
+      <p>No app da Caju, os saldos são identificados separadamente: Conta Digital, Saldo Multi e as categorias tradicionais. Assim, você sempre saberá qual saldo está sendo utilizado. </p>
+      <br/>
+      <p>As transações no cartão da Caju sempre vão priorizar usar primeiro o saldo das categorias de benefícios, mas caso não haja saldo suficiente, então o cartão vai consultar se há saldo na categoria Saldo Multi e por último, se não houver saldo suficiente nas categorias incluindo o Saldo Multi ou se a natureza da transação não estiver dentro das categorias de benefícios, então cartão vai tentar passar a transação no saldo da Conta Digital e uma vez que haja saldo suficiente, a transação será realizada com sucesso, caso não haja saldo, a transação não será realizada.</p>
+`
+    },
+    {
+      title: 'Posso transferir saldos entre as categorias antigas e o Saldo Multi?',
+      htmlContent: `Não, não é possível transferir saldos entre as categorias e o Saldo Multi.`
+    },
+  ],
+  colaborador4: [
+    {
+      title: 'O saldo da Conta Digital pode ser resgatado pela empresa?',
+      htmlContent: `Não. Após o depósito, o saldo é seu e não pode ser cancelado ou resgatado pela empresa.`
+    },
+    {
+      title: 'O dinheiro da Conta Digital rende?',
+      htmlContent: `Não. No momento a Conta Digital não oferece rendimento, mas permite total liberdade de uso do saldo.`
+    },
+    {
+      title: 'A Conta digital é igual a uma conta salário?',
+      htmlContent: `Não. A Conta Digital não é uma conta salário. Ela pode ser usada para receber valores como auxílio-creche, ajuda de custo, premiações, entre outros. Vale ressaltar que a conta digital da Caju é uma conta de pagamento que pode receber recursos decorrentes de portabilidade salarial`
+    },
+    {
+      title: 'O que acontece se eu não ativar o Pix?',
+      htmlContent: `Você ainda poderá usar a Conta para pagar boletos, fazer compras e sacar dinheiro, mas o Pix ficará desabilitado até que você o ative.`
+    },
+    {
+      title: 'Onde posso tirar dúvidas se algo não funcionar?',
+      htmlContent: `Você pode falar com o time de atendimento da Caju via WhatsApp, telefone ou e-mail. Eles estão prontos para ajudar com qualquer dúvida sobre a Conta Digital ou o Saldo Multi.`
+    },
+  ],
+  
+}
+
+/* Collapse */
+document.getElementById('faqId').querySelectorAll('[faqItem]').forEach(obj => {
+  let trocarRotacaoSeta = false
+
+  obj.addEventListener('click', (event) => {
+    if(event.target.classList[0] == 'faq__header') {
+      obj.querySelector('.faq__content').style.display = trocarRotacaoSeta ? '' : 'flex' 
+      obj.querySelector('[arrowRed]').style.transform = `rotate(${trocarRotacaoSeta ? 0 : 180}deg)` 
+      trocarRotacaoSeta = !trocarRotacaoSeta
+    }
+  })
+})
+
+document.querySelectorAll('collapse').forEach(obj => {
+  const alturaDoHeader = obj.getAttribute('alturaDoHeader')
+  const valorDeReferenciaKey = obj.getAttribute('valorDeReferencia')
+
+  data[valorDeReferenciaKey].forEach(element => {
+    
+    let abertoFechado = false
+    const card = document.createElement('collapse-card')
+    card.setAttribute('role', 'button')
+    card.innerHTML = `
+      <collapse-header style="height: ${alturaDoHeader}px">
+        ${element.title}
+        <collapse-arrow></collapse-arrow>
+      </collapse-header>
+      <collapse-content>${element.htmlContent}</collapse-content>
+    `
+
+    card.addEventListener('click', (e) => {
+      abertoFechado ? e.currentTarget.classList.remove('collapse--open') : e.currentTarget.classList.add('collapse--open')
+      abertoFechado = !abertoFechado
+    })
+
+    obj.appendChild(card)
+  })
+})
